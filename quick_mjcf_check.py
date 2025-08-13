dea47d319afa4f87a31c02669527f0e1d54ae966
Kotaro Kajitsuka <tactile.kajitsuka@aist.go.jp>
2025-08-13 15:23:01 +0900

    switch frame

--- quick_mjcf_check.py
+++ quick_mjcf_check.py
@@ -1,6 +1,6 @@
 # quick_mjcf_check.py
 import os, traceback, mujoco
-path = "mujoco_playground/_src/manipulation/my_leap_hand/xmls/ur5e_leap_menagerie_scene.xml"
+path = "mujoco_playground/_src/manipulation/my_leap_hand/xmls/leap_hand_6dof_ctrl_scene.xml"
 print("Loading:", os.path.abspath(path))
 try:
     mujoco.MjModel.from_xml_path(path)
